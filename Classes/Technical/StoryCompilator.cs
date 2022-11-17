1801ee809813da644b4d153b383842b57e1aabe6
Sergat 777 <sergey.kond777@gmail.com>
2022-11-17 05:58:24 +0300

    realize commands

--- Classes/Technical/StoryCompilator.cs
+++ Classes/Technical/StoryCompilator.cs
@@ -11,6 +11,19 @@ namespace SKA_Novel.Classes.Technical
         public static int LineOfStory { get; set; } = -1;
         public static string[] CurrentStory { get; set; }
 
+        public delegate void Command(string codeString);
+
+        //{"ChangeBackground", "Изменение заднего фона, принимает имя фона"},
+        //{"ChangeMusic", "Изменение музыки, принимает имя музыки (.wav)"},
+        //{ "GoNextFile", "Переход к новому файлу, принимает имя файла"}
+
+        public static Dictionary<String, Command> Commands { get; } = new Dictionary<string, Command>()
+        {
+            {"SetBackground", SetBackground},
+            {"SetMusic", SetMusic},
+            {"GoNextFile", GoNextFile}
+        };
+
         public static string GetNextLine()
         {
             if ((LineOfStory + 1) < CurrentStory.Count())
@@ -26,18 +39,34 @@ namespace SKA_Novel.Classes.Technical
         {
             string command = codeString.Substring(1, codeString.IndexOf(':') - 1);
 
-            if (command == "ChangeBackground")
-                ChangeBackground(codeString);
+            foreach (string commandName in Commands.Keys)
+                if (command == commandName)
+                    Commands[command](codeString);
 
             GetNextLine();
         }
 
-        private static void ChangeBackground(string codeString)
+        private static string GetArguments(string codeString)
         {
-            string backgroundName = codeString.Substring(
+            return codeString.Substring(
                 codeString.IndexOf(':') + 1,
-                codeString.Length - codeString.IndexOf(':') - 1);
-            MediaHelper.SetBackground(backgroundName);
+                codeString.Length - (codeString.IndexOf(':') + 1));
+        }
+
+        private static void SetBackground(string codeString)
+        {
+            MediaHelper.SetBackground(GetArguments(codeString));
+        }
+
+        private static void SetMusic(string codeString)
+        {
+
+        }
+
+        private static void GoNextFile(string codeString)
+        {
+            CurrentStory = MediaHelper.BeatStringToLines(MediaHelper.GetTextFromFile(GetArguments(codeString)));
+            LineOfStory = -1;
         }
     }
 }
